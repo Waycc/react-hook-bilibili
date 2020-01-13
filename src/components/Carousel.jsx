fcf38092c3ad2fd8132c2f32598ac9a8128b90a6
wei_chao <550974053@qq.com>
2020-01-13 14:33:42 +0800

    重写轮播图组件，解决轮播时图片衔接有问题的bug

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,213 +1,260 @@
-import React, {Children, useCallback, useEffect, useRef, useState} from "react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import './style/carousel.scss';
 
 let defaultProps = {
   infinite: true,
   autoPlay: true,
-  autoPlayTime: 2000,
+  interval: 2000,
   duration: 0.3
 };
 
-let Carousel = function (props) {
-  let innerRef = useRef(null);
-  let startXRef = useRef(0);
-  let timeOutRef = useRef(null);
-  let movingRef = useRef(false);
+export default function Carousel(props) {
+  let {children, interval, duration} = props;
+  let [currentIndex, setCurrentIndex] = useState(0);
+  let [carouselItems, setCarouselItems] = useState([]);
   let [viewWidth, setViewWidth] = useState(0);
-  let [autoPlay, setAutoPlay] = useState(props.autoPlay);
-  let [currentIndex, setCurrentIndex] = useState(1);
-  let [innerItems, setInnerItems] = useState(null); // 经过处理后的元素
-
-  let childrenCount = React.Children.count(props.children);
-  let innerItemsCount = childrenCount > 1 ? childrenCount + 2 : childrenCount;
-  let infiniteSlide = childrenCount > 1 ? props.infinite : false;
-  let autoPlayTime = props.autoPlayTime;
-  let duration = props.duration + 's';
-  let innerWidth = innerRef.current && innerRef.current.offsetWidth || 0
-
-  let startAutoPlay = () => {
-    setAutoPlay(true)
-  };
-
-  let stopAutoPlay = () => {
-    clearTimeout(timeOutRef.current)
-    setAutoPlay(false)
-  };
-
-  // 自动播放轮播图
-  useEffect(() => {
-    if (autoPlay && viewWidth && childrenCount > 1) {
-      if (timeOutRef.current) clearTimeout(timeOutRef.current);
-      timeOutRef.current = setTimeout(() => {
-        innerRef.current.style.transition = 'none';
-        move(0);
-        setTimeout(() => {
-          finalMove('left', viewWidth, viewWidth / 2);
-        }, 0);
-      }, autoPlayTime);
-    }
-    return () => {
-      clearTimeout(timeOutRef.current)
-    }
-  });
+  let carouselItemContainer = useRef();
+  let startX = useRef(0);
+  let moveDistanceRef = useRef(0);
+  let viewWidthRef = useRef();
+  let carouselItemsRef = useRef([]);
+  let currentIndexRef = useRef(0);
+  currentIndexRef.current = currentIndex;
+  carouselItemsRef.current = carouselItems;
+  viewWidthRef.current = viewWidth;
+  let moving = useRef(false);
+  let middleRef = useRef();
+  let rightRef = useRef();
+  let leftRef = useRef();
+  let nextPalyTimeout = useRef(null);
+  let autoPalyAnimat = useRef(null);
+  let finalMoveAnimat = useRef(null);
+  let originCarouselItemsRef = useRef([]);
 
   useEffect(() => {
-    innerRef.current.addEventListener('touchstart', touchStart);
-    innerRef.current.addEventListener('touchmove', touchMove);
-    innerRef.current.addEventListener('touchend', touchEnd);
-    window.addEventListener('resize', computeEachItemWidth)
-    if (childrenCount === 1) {
-      setCurrentIndex(0);
-    }
+    let ele = carouselItemContainer.current;
+    ele.addEventListener('touchstart', onTouchStart);
+    ele.addEventListener('touchmove', onTouchMove);
+    ele.addEventListener('touchend', onTouchEnd);
+
     return () => {
-      innerRef.current.removeEventListener('touchstart', touchStart);
-      innerRef.current.removeEventListener('touchmove', touchMove);
-      innerRef.current.removeEventListener('touchend', touchEnd);
-      window.removeEventListener('resize', computeEachItemWidth)
+      ele.removeEventListener('touchstart', onTouchStart);
+      ele.removeEventListener('touchmove', onTouchMove);
+      ele.removeEventListener('touchend', onTouchEnd);
     };
-  });
-
-  // 获取可视区域大小
-  useEffect(() => {
-    computeEachItemWidth();
-  }, [innerItemsCount]);
-
-  // 第一次进入时初始化图片位置
-  useEffect(() => {
-    if (viewWidth) {
-      let index = childrenCount <= 1 ? 0 : 1;
-      move(0, index);
-    }
-  }, [viewWidth, props.children]);
+  }, []);
 
   useEffect(() => {
-    // 将原来的children中的第一个元素和最后一个元素，分别复制后放在末尾和头部，方便无限轮播
-    if (viewWidth) {
-      let firstEle = null;
-      let lastEle = null;
-
-      let newChildren = React.Children.map(props.children, (child, index) => {
-        child = React.cloneElement(child, {style: {width: '100%', height: '100%', display: 'inline-block'}});
-        let innerItem = (
-          <div key={index + 1} style={{width: viewWidth}} className={'carousel-item'}>
-            {child}
-          </div>
-        );
-        if (index === 0 && childrenCount > 1) firstEle = React.cloneElement(innerItem, {key: 0});
-        if (index === childrenCount - 1 && childrenCount > 1) lastEle = React.cloneElement(innerItem, {key: childrenCount + 1});
-        return innerItem;
-      });
-      let innerItems = [lastEle, ...newChildren, firstEle];
-      setInnerItems(innerItems);
+    let carouselItems = React.Children.map(children, child => {
+      return React.cloneElement(child, {style: {width: '100%', height: '100%'}});
+    });
+    originCarouselItemsRef.current = [...carouselItems];
+    if (carouselItems.length === 2) {
+      carouselItems.push(carouselItems[1]);
     }
-  }, [viewWidth, props.children]);
+    setCarouselItems(carouselItems);
+  }, [children]);
 
-  // 计算轮播图可视区域的大小
-  let computeEachItemWidth = () => {
-    let viewWidth = innerRef.current.offsetWidth / innerItemsCount;
-    if (!innerItemsCount) {
-      viewWidth = 0;
-    }
-    setViewWidth(viewWidth);
+  useEffect(() => {
+    setViewWidth(carouselItemContainer.current.offsetWidth);
+  }, []);
+
+  useEffect(() => {
+    startNextPlay();
+  }, []);
+
+  useEffect(() => () => {
+    clearTimeout(nextPalyTimeout.current);
+    cancelAnimationFrame(finalMoveAnimat.current);
+    cancelAnimationFrame(autoPalyAnimat.current);
+  }, []);
+
+  let isTwoChildren = () => {
+    return originCarouselItemsRef.current.length === 2;
   };
 
-  // 屏幕触摸时的移动
-  let move = (x, index) => {
-    index = index || currentIndex;
-    let moveX = -(index * viewWidth) + x;
-    innerRef.current.style.transform = `translateX(${moveX}px)`;
+  let startNextPlay = () => {
+    clearTimeout(nextPalyTimeout.current);
+    nextPalyTimeout.current = setTimeout(autoPlay, interval);
   };
 
-  // 触摸结束后，整个图片的移动
-  let finalMove = (direction, distance, changeX) => {
-    movingRef.current = true;
-    innerRef.current.style.transition = `transform ${duration}`;
-    let nextIndex = direction === 'left' ? currentIndex + 1 : currentIndex - 1;
-    distance = Math.abs(distance);
-    if (nextIndex > innerItemsCount - 1 || nextIndex < 0 || Math.abs(changeX) < viewWidth / 4) {
-      nextIndex = currentIndex;
-      distance = 0;
+  let autoPlay = () => {
+    if (carouselItemsRef.current.length <= 1) return;
+    let moveStep = getMoveStep();
+    moveDistanceRef.current = moveDistanceRef.current - moveStep;
+    moveEle(moveDistanceRef.current);
+    if (Math.abs(moveDistanceRef.current) < viewWidthRef.current / 4) {
+      requestAnimationFrame(autoPlay);
+      return;
     }
-    if (direction === 'left') {
-      move(-distance);
+    finalMove();
+  };
+
+  let getLeftMiddleRightIndex = () => {
+    let childrenLength = carouselItemsRef.current.length;
+    let currentIndex = currentIndexRef.current;
+    let left,
+      middle = currentIndex,
+      right;
+    if (currentIndex === 0) {
+      right = currentIndex + 1;
+      left = childrenLength - 1;
+    } else if (currentIndex === childrenLength - 1) {
+      right = 0;
+      left = currentIndex - 1;
     } else {
-      move(distance);
+      right = currentIndex + 1;
+      left = currentIndex - 1;
     }
-    if (infiniteSlide) {
-      if (nextIndex === 0) {
-        setPosition(innerItemsCount - 2);
-      }
-      if (nextIndex === innerItemsCount - 1) {
-        setPosition(1);
-      }
-    }
-    setCurrentIndex(nextIndex);
-    movingRef.current = false;
+    return {left, middle, right};
   };
 
-  let setPosition = (nextIndex) => {
-    setTimeout(() => {
-      // 设置正确索引，下一次touchStart事件触发时，会正确设置translateX的值
-      // innerRef.current.style.transition = 'none';
-      setCurrentIndex(nextIndex);
-      // move(0, nextIndex)
-    }, 0);
+  let renderCarouselItems = () => {
+    let {middle, left, right} = getLeftMiddleRightIndex();
+    return carouselItems.map((ci, index) => {
+      switch (index) {
+        case middle:
+          return <div className={'carousel-item'} style={{transform: `translateX(0px)`}} ref={middleRef}>{ci}</div>;
+        case left:
+          return <div className={'carousel-item'} style={{transform: `translateX(${-viewWidth}px)`}}
+                      ref={leftRef}>{ci}</div>;
+        case right:
+          return <div className={'carousel-item'} style={{transform: `translateX(${viewWidth}px)`}}
+                      ref={rightRef}>{ci}</div>;
+
+        default:
+          if (index > middle) {
+            return <div className={'carousel-item'} style={{transform: `translateX(${viewWidth}px)`}}>{ci}</div>;
+          } else {
+            return <div className={'carousel-item'} style={{transform: `translateX(${-viewWidth}px)`}}>{ci}</div>;
+          }
+      }
+    });
   };
 
-  let touchStart = (e) => {
+  let onTouchStart = (e) => {
     e.stopPropagation();
-    stopAutoPlay();
-    innerRef.current.style.transition = 'none';
-    startXRef.current = e.changedTouches[0].pageX;
-    move(0);
+    clearTimeout(nextPalyTimeout.current);
+    startX.current = e.touches[0].clientX;
   };
 
-  let touchMove = (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    let x = e.changedTouches[0].pageX;
-    move(x - startXRef.current);
+  let onTouchMove = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (moving.current) return;
+    let {clientX} = e.touches[0];
+    moveDistanceRef.current = clientX - startX.current;
+    moveEle(moveDistanceRef.current);
   };
 
-  let touchEnd = (e) => {
-    e.stopPropagation()
-    let endX = e.changedTouches[0].pageX;
-    let changeX = endX - startXRef.current;
-    // 触摸滑动方向
-    let direction = changeX < 0 ? 'left' : 'right';
-    finalMove(direction, viewWidth, changeX);
-    startXRef.current = 0;
-    startAutoPlay()
+  let onTouchEnd = (e) => {
+    e.stopPropagation();
+    if (moving.current) return;
+    finalMove();
+  };
+
+  let getMoveStep = () => {
+    return viewWidthRef.current / (duration * 1000 / 16);
+  };
+
+  let moveEle = (distance) => {
+    let viewWidth = viewWidthRef.current;
+    let middleEle = middleRef.current,
+      leftEle = leftRef.current,
+      rightEle = rightRef.current;
+    if (middleEle) middleEle.style.transform = `translateX(${distance}px)`;
+    if (leftEle) leftEle.style.transform = `translateX(${-viewWidth + distance}px)`;
+    if (rightEle) rightEle.style.transform = `translateX(${viewWidth + distance}px)`;
+  };
+
+  let getCorrectMoveStep = ({moveStep, mouseDirection, isBack}) => {
+    if (mouseDirection === 'left' && !isBack) {
+      moveStep = -moveStep;
+    }
+    if (isBack && mouseDirection === 'right') {
+      moveStep = -moveStep;
+    }
+    return moveStep;
+  };
+
+  let getCorrectDistance = ({viewWidth, isBack, moveStep, moveDistance, mouseDirection}) => {
+    let finalDistance = moveDistance + moveStep;
+    if (isBack) {
+      if (moveStep < 0 && finalDistance < 0) {
+        finalDistance = 0;
+      }
+      if (moveStep > 0 && finalDistance > 0) {
+        finalDistance = 0;
+      }
+    }
+
+    if (!isBack && Math.abs(finalDistance) > viewWidth) {
+      finalDistance = mouseDirection === 'left' ? -viewWidth : viewWidth;
+    }
+    return finalDistance;
+  };
+
+  let handle2Ele = ({nextIndex: middle}) => {
+    let currentEle = carouselItemsRef.current[middle];
+    let otherEle = carouselItemsRef.current.find((item) => item !== currentEle);
+    carouselItemsRef.current.forEach((item, index) => {
+      if (middle !== index) {
+        carouselItemsRef.current[index] = otherEle;
+      }
+    });
+    setCarouselItems([...carouselItemsRef.current]);
+  };
+
+  let finalMove = () => {
+    moving.current = true;
+    let moveStep = getMoveStep();
+    let moveDistance = moveDistanceRef.current;
+    let viewWidth = viewWidthRef.current;
+    let mouseDirection = moveDistance > 0 ? 'right' : 'left';
+    let isBack = carouselItemsRef.current.length <= 1 ? true : Math.abs(moveDistance) < viewWidth / 4;
+    let finalDistance, finishWidth;
+
+    moveStep = getCorrectMoveStep({moveStep, mouseDirection, isBack});
+    finalDistance = getCorrectDistance({viewWidth, isBack, mouseDirection, moveDistance, moveStep});
+    moveEle(finalDistance);
+    moveDistanceRef.current = finalDistance;
+
+    finishWidth = isBack ? 0 : viewWidth;
+    if (Math.abs(finalDistance) === finishWidth) {
+      let {left, middle, right} = getLeftMiddleRightIndex();
+      !isBack && setCurrentIndex(currentIndex => {
+        let nextIndex = mouseDirection === 'left' ? right : left;
+        isTwoChildren() && handle2Ele({nextIndex});
+        return nextIndex;
+      });
+      moveDistanceRef.current = 0;
+      moving.current = false;
+      startNextPlay();
+      // setTimeout(() => handle2Ele({left, middle, right}), 0)
+      return;
+    }
+    finalMoveAnimat.current = requestAnimationFrame(finalMove);
   };
 
   return (
-    <div className={'carousel-outer-container'} >
-      <div className={'carousel-wrap'}>
-        <div
-          ref={innerRef}
-          className={'carousel-inner-container'}
-          style={{width: `${100 * innerItemsCount}%`}}
-        >
-          {innerItems}
-        </div>
+    <div className={'my-carousel'}>
+      <div className={'carousel-item-container'} ref={carouselItemContainer}>
+        {renderCarouselItems()}
       </div>
       {
-        childrenCount === 0 ? null :
-        <div className={'carousel-button-wrap '}>
-          {
-            [...new Array(childrenCount).keys()].map(key => {
-              key = childrenCount > 1 ? key + 1 : key;
-              return (
-                <span key={key} className={`carousel-button ${currentIndex === key ? 'selected' : ''}`}/>
-              )
-            })
-          }
-        </div>
+        carouselItems.length === 0 ? null :
+          <div className={'carousel-button-wrap '}>
+            {
+              originCarouselItemsRef.current.map(item => {
+                return (
+                  <span className={`carousel-button ${item === carouselItems[currentIndex] ? 'selected' : ''}`}/>
+                )
+              })
+            }
+          </div>
       }
     </div>
   );
-};
+}
 
 Carousel.defaultProps = defaultProps;
-
-export default Carousel;
